b7bd9b2ba6ed2bd819d693a97f5662c3655bfc7c
Hari Narayanan <hnarayanan5@gmail.com>
2020-07-22 17:13:43 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import React from 'react';
-import Landing from './Components/Master/Landing';
+import React from "react";
+import Landing from "./Components/Master/Landing";
 import Blog from "./Components/Master/Blog";
 import Dashboard from "./Components/Master/Dashboard";
-import "./App.css"
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-
-  } from "react-router-dom";
+import "./App.css";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-	return (
-		<Router>
-		 <Switch>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route exact path="/blog">
-           <Blog />
-          </Route>
-		  <Route exact path="/dashboard">
-           <Dashboard />
-          </Route>
-         
-        </Switch>
-		</Router>
-	);
+  return (
+    <Router basename = {process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route exact path="/blog">
+          <Blog />
+        </Route>
+        <Route exact path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
